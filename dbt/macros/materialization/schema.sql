2e6d2b5f1d4b5577219d694dce0e7b11d522b877
Dmitry Maslennikov <mrdaimor@gmail.com>
2023-05-26 09:43:43 +0400

    target-horizon-months added to schema

--- dbt/macros/materialization/schema.sql
+++ dbt/macros/materialization/schema.sql
@@ -1,3 +1,3 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {{ target.schema ~ '_' ~ dbt_utils.slugify(var('target')) }}
+    {{ target.schema ~ '_' ~ dbt_utils.slugify(var('target')) ~ '_m' ~ var('target-horizon-months') }}
 {%- endmacro %}
