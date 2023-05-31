be38e7d8d6fc42e287206501a0fe233d894e5c06
Dmitry Maslennikov <mrdaimor@gmail.com>
2023-05-31 16:29:56 -0400

    model predict

---
+++ dbt/models/mlready/predict.sql
@@ -0,0 +1,19 @@
+-- depends_on: {{ ref('train') }}
+
+{{
+    config(
+        materialized='table',
+        indexes=[
+            {'name': 'KeyIdx', 'columns': ['Key'], 'unique': True},
+            {'name': 'TargetIdx', 'columns': ['target']},
+        ]
+    )
+}}
+{%- set model_name = this.schema ~ '_train' %}
+
+select
+    Key,
+    target,
+    predict({{ model_name }}) as predicted,
+    probability({{ model_name }}) as probability
+from {{ ref('summary') }}
