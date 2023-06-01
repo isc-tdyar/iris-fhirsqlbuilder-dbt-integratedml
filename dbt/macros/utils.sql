aa5292553bf286712654d2ece87b40f9d964a880
Dmitry Maslennikov <mrdaimor@gmail.com>
2023-06-01 14:57:58 -0400

    updated mlmodel materialization

--- dbt/macros/utils.sql
+++ dbt/macros/utils.sql
@@ -4,3 +4,17 @@
 {%- endif -%}
 {{ return(rel.schema ~ '.' ~ rel.identifier) }}
 {% endmacro  %}
+
+{% macro getdoc(el) %}
+{{ print('getdoc') }}
+{{ print(el) }}
+{{ return('test') }}
+{% endmacro %}
+
+{% macro code_to_col(codes) %}
+{% set result = [] %}
+{% for code in codes %}
+{% do result.append(modules.re.sub('-', '_', code)) %}
+{% endfor %}
+{{ return(result) }}
+{% endmacro %}
--- dbt/models/mlready/train.sql
+++ dbt/models/mlready/train.sql
@@ -1,24 +1,10 @@
 -- depends_on: {{ ref('lhs-lc-ml-codes') }}
 -- depends_on: {{ ref('lhs-stroke-ml-codes') }}
 
-{{ 
-    config(
-        materialized='mlmodel',
-        predicting='target',
-        indexes=[
-            {'name': 'KeyIdx', 'columns': ['Key'], 'unique': True},
-            {'name': 'TargetIdx', 'columns': ['target']},
-        ]
-    ) 
-}}
-
-{% set columns = dbt_utils.get_column_values(target_lhs_dataset(), 'code') %}
-
+{# {% set columns = dbt_utils.get_column_values(target_lhs_dataset(), 'code') %} #}
+{# {{ print(columns) }} #}
 SELECT
-    "Key", "target",
-	{%- for col in columns %}
-    {{ adapter.quote(modules.re.sub('[ -]+', '_', col)) }}{% if not loop.last %},{% endif -%}
-    {% endfor %}
+    * 
 FROM {{ ref('summary') }} WHERE 
 	(target = 0 and %ID %FIND %SQL.SAMPLE( '{{unquotedref('summary')}}' , {{var('split-train-pct')}} , {{var('split-seed')}} ))
 	OR 
